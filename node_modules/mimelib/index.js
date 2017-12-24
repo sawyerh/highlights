7296537f7ac2bbd000274b016827950877a14351
sawyerh <s.hollenshead@gmail.com>
2017-12-24 12:16:22 -0500

    Remove node_modules

--- node_modules/mimelib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = require("./lib/mimelib");
-module.exports.contentTypes = require("./lib/content-types");
-module.exports.contentTypesReversed = require("./lib/content-types-reversed");
