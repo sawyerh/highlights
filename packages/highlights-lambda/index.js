5d7ce3cd06ab5cfb7c90b0ac441992c4a6cd9bc6
sawyerh <s.hollenshead@gmail.com>
2017-12-25 13:46:29 -0500

    Refactor lambda handler

--- packages/highlights-lambda/index.js
+++ packages/highlights-lambda/index.js
@@ -1,54 +1,44 @@
-const aws = require("aws-sdk");
-const MailParser = require("mailparser").MailParser;
-const parseMail = require("./parsers/parser");
-const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+const AWS = require("aws-sdk");
+const importMail = require("./importer");
+const s3 = new AWS.S3();
 
+/**
+ * Lambda handler fired when a new S3 file has been created.
+ * This file is expected to be an email message conforming
+ * to one of the supported highlight exports (Kindle or
+ * or plain text).
+ * @param {Object} event
+ * @param {Array<Object>} event.Records
+ * @param {Object} context
+ * @param {Boolean} debug - Disable saving the highlights to Siteleaf/Firebase
+ */
 exports.handler = function(event, context, debug = false) {
   console.log("Received event", event);
+  const params = s3Params(event);
 
-  // Get the object from the event and show its content type
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return context.fail(
+        `Error getting object ${params.Key} from bucket ${params.Bucket}`
+      );
+    }
+
+    importMail(data.Body, debug)
+      .then(() => context.succeed(`Successfully imported ${params.Key}`))
+      .catch(context.fail);
+  });
+};
+
+/**
+ * Form the request object for getObject
+ * @param {Object} event - Lambda Event
+ * @returns {Object}
+ */
+function s3Params(event) {
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(
     event.Records[0].s3.object.key.replace(/\+/g, " ")
   );
-  const params = { Bucket: bucket, Key: key };
-  const mailparser = new MailParser();
-
-  mailparser.on("end", function(mail) {
-    parseMail(mail)
-      .then(function() {
-        context.succeed("Parsed mail");
-      })
-      .catch(function(message) {
-        context.fail(message);
-      });
-  });
-
-  console.log("Handling %s", key);
-
-  try {
-    s3
-      .getObject(params, function(err, data) {
-        if (err) {
-          console.log(err);
-          const message =
-            "Error getting object " +
-            key +
-            " from bucket " +
-            bucket +
-            ". Make sure they exist and your bucket is in the same region as this function.";
-          console.log(message);
-          context.fail(message);
-        }
-      })
-      .on("httpData", function(chunk) {
-        mailparser.write(chunk);
-      })
-      .on("httpDone", function() {
-        mailparser.end();
-      });
-  } catch (e) {
-    console.log("Caught Error:");
-    context.fail(e.message);
-  }
-};
+  return { Bucket: bucket, Key: key };
+}
