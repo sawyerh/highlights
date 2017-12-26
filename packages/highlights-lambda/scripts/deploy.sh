8b4a9edeca1d06033ddc60e119d6f1bdc5dd1f98
sawyerh <s.hollenshead@gmail.com>
2017-12-26 14:24:38 -0500

    Add ZIP step to deploy script

--- packages/highlights-lambda/scripts/deploy.sh
+++ packages/highlights-lambda/scripts/deploy.sh
@@ -2,17 +2,21 @@
 
 set -e
 
+RED="\033[0;31m"
 GREEN='\033[0;32m'
+BLUE="\033[0;34m ℹ "
 NC='\033[0m' # No color
 
-echo "${GREEN}Clean up...${NC}"
+echo "${BLUE}Clean up...${NC}"
 trash ./dist
 mkdir ./dist
 
-echo "${GREEN}Copy files...${NC}"
+echo "${BLUE}Copy files...${NC}"
 cp ./*.{js,json,lock} ./dist
 cp -R ./importer ./dist
 
-echo "${GREEN}Install production dependencies...${NC}"
+echo "${BLUE}Install production dependencies...${NC}"
 cd ./dist
 yarn install --production
+zip -X -r -q ../handler.zip *
+cd ..
