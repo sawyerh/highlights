867b3eee8d6c2b6f6be973399fc0fcd5fb890657
sawyerh <s.hollenshead@gmail.com>
2017-12-26 10:54:58 -0500

    Convert Volume to Class

--- packages/highlights-lambda/importer/Volume.js
+++ packages/highlights-lambda/importer/Volume.js
@@ -1,44 +1,47 @@
 const db = require("./firestore")();
-const Volume = {};
+const Firestore = require("@google-cloud/firestore");
 
-/**
- * @param {Objects} props
- * @property {String} props.title
- * @property {String} props.importTitle
- * @property {Array<String>} props.authors
- * @returns {Promise<Object>} Snapshot
- */
-Volume.create = function(props) {
-  return db.collection("volumes").add({
-    authors: props.authors,
-    importTitle: props.title,
-    title: props.title
-  });
-};
+class Volume {
+  /**
+   * @param {Object} props
+   * @param {String} props.title
+   * @param {String} props.importTitle
+   * @param {Array<String>} props.authors
+   * @returns {Promise<Object>} Snapshot
+   */
+  static create(props) {
+    return db.collection("volumes").add({
+      authors: props.authors,
+      createdAt: Firestore.FieldValue.serverTimestamp(),
+      importTitle: props.title,
+      title: props.title
+    });
+  }
 
-/**
- * @param {String} title
- * @returns {Promise<Object>} Snapshot
- */
-Volume.findByImportTitle = function(title) {
-  return db
-    .collection("volumes")
-    .where("importTitle", "==", title)
-    .get();
-};
+  /**
+   * @param {String} title
+   * @returns {Promise<Object>} Snapshot
+   */
+  static findByImportTitle(title) {
+    return db
+      .collection("volumes")
+      .where("importTitle", "==", title)
+      .get();
+  }
 
-/**
- * @param {Object} props
- * @property {String} props.title
- * @returns {Promise<Object>} Resolves with volume object
- */
-Volume.findOrCreate = function(props) {
-  return this.findByImportTitle(props.title).then(snapshot => {
-    if (snapshot.empty) {
-      return this.create(props);
-    }
-    return snapshot.docs[0].ref;
-  });
-};
+  /**
+   * @param {Object} props
+   * @param {String} props.title
+   * @returns {Promise<Object>} Resolves with volume object
+   */
+  static findOrCreate(props) {
+    return this.findByImportTitle(props.title).then(snapshot => {
+      if (snapshot.empty) {
+        return this.create(props);
+      }
+      return snapshot.docs[0].ref;
+    });
+  }
+}
 
 module.exports = Volume;
