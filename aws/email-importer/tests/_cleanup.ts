c7a8f94aa3b3a546fb7e60371d9771cd2620552c
Sawyer <git@sawyerh.com>
2022-12-26 14:44:32 -0800

    Fix Firebase emulator cleanup in Node 18

--- aws/email-importer/tests/_cleanup.ts
+++ aws/email-importer/tests/_cleanup.ts
@@ -6,7 +6,7 @@ export default async function cleanup(projectId: string) {
 	// Clear the emulator database
 	// https://firebase.google.com/docs/emulator-suite/connect_firestore#clear_your_database_between_tests
 	await fetch(
-		`http://localhost:${firebase.emulators.firestore.port}/emulator/v1/projects/${projectId}/databases/(default)/documents`,
+		`http://127.0.0.1:${firebase.emulators.firestore.port}/emulator/v1/projects/${projectId}/databases/(default)/documents`,
 		{
 			method: "DELETE",
 		},
--- firebase/functions/tests/_cleanup.ts
+++ firebase/functions/tests/_cleanup.ts
@@ -6,7 +6,7 @@ export default async function cleanup() {
 	// Clear the emulator database
 	// https://firebase.google.com/docs/emulator-suite/connect_firestore#clear_your_database_between_tests
 	await fetch(
-		`http://localhost:8080/emulator/v1/projects/${projectId}/databases/(default)/documents`,
+		`http://127.0.0.1:8080/emulator/v1/projects/${projectId}/databases/(default)/documents`,
 		{
 			method: "DELETE",
 		},
