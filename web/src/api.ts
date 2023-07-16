d6f8d75c0939c5fded3b9242923a05e928fddc4f
Sawyer <git@sawyerh.com>
2023-07-16 13:29:20 -0700

    Create generic fetch wrapper

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,21 +1,37 @@
+/**
+ * @file Data fetching methods for use only on the server.
+ * Methods return the data with the appropriate type.
+ */
 import "server-only";
 
 const API_URL = process.env.API_URL;
 
+// Volume covers or titles are often modified after initial import,
+// so allow a revalidation period.
+const VOLUME_REVALIDATION_TIME = 60 * 10; // 10 minutes
+
 /**
  * Wrapper around `fetch` that returns the JSON body.
- * @example request('volumes');
+ * @example request('https://example.com/api/v1/users')
  */
-async function request(apiRoute: string, options?: RequestInit) {
-	const response = await fetch(`${API_URL}/${apiRoute}`, options);
+async function request(path: string, options?: RequestInit) {
+	const response = await fetch(path, options);
 	const json = await response.json();
 	return json;
 }
 
+/**
+ * Send a request to a serverless HTTP endpoint in Firebase.
+ * @example firebaseRequest('volumes')
+ */
+async function firebaseRequest(route: string, options?: RequestInit) {
+	return request(`${API_URL}/${route}`, options);
+}
+
 export async function getVolumes() {
-	const { data } = await request("volumes", {
+	const { data } = await firebaseRequest("volumes", {
 		next: {
-			revalidate: 60 * 10, // 10 minutes
+			revalidate: VOLUME_REVALIDATION_TIME,
 		},
 	});
 
@@ -23,19 +39,23 @@ export async function getVolumes() {
 }
 
 export async function getVolume(volumeId: string) {
-	const volume = await request(`volumes/${volumeId}`);
+	const volume = await firebaseRequest(`volumes/${volumeId}`, {
+		next: {
+			revalidate: VOLUME_REVALIDATION_TIME,
+		},
+	});
 
 	return volume as Volume;
 }
 
 export async function getHighlights(volumeId: string) {
-	const { data } = await request(`highlights?volume=${volumeId}`);
+	const { data } = await firebaseRequest(`highlights?volume=${volumeId}`);
 
 	return data as Highlight[];
 }
 
 export async function getHighlight(highlightId: string) {
-	const highlight = await request(`highlights/${highlightId}`);
+	const highlight = await firebaseRequest(`highlights/${highlightId}`);
 
 	return highlight as Highlight;
 }
