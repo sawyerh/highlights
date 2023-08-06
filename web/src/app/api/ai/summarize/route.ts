9fee8a997a0d64db6dd32cef6ac2adf21b1e02fc
Sawyer <git@sawyerh.com>
2023-08-06 16:49:18 -0700

    Request summarization upon render

--- web/src/app/api/ai/summarize/route.ts
+++ web/src/app/api/ai/summarize/route.ts
@@ -9,6 +9,11 @@ const AI_URL = process.env.AI_URL;
  */
 export async function GET(req: NextRequest) {
 	const volume_id = req.nextUrl.searchParams.get("volume_id");
+	// Vercel free plan has a 10 second max timeout, which is shorter
+	// than what this endpoint takes to run sometimes. So we allow
+	// passing this parameter to hit the endpoint without waiting for
+	// it to finish, as a way to pre-warm the endpoint.
+	const quick_exit = req.nextUrl.searchParams.get("quick_exit");
 
 	if (!volume_id) {
 		return NextResponse.json(
@@ -17,7 +22,7 @@ export async function GET(req: NextRequest) {
 		);
 	}
 
-	const { data } = await request<{ data: SummarizationResult[] }>(
+	const requestPromise = request<{ data: SummarizationResult[] }>(
 		`${AI_URL}/summarize/${volume_id}`,
 		{
 			next: {
@@ -25,6 +30,12 @@ export async function GET(req: NextRequest) {
 			},
 		},
 	);
+
+	if (quick_exit) {
+		return NextResponse.json([]);
+	}
+
+	const { data } = await requestPromise;
 
 	return NextResponse.json(data);
 }
