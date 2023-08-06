23ff3705f4d09536406ee64f72b8e470ac903b13
Sawyer <git@sawyerh.com>
2023-08-05 17:36:10 -0700

    Fix ts

--- aws/email-importer/src/types.ts
+++ aws/email-importer/src/types.ts
@@ -1,20 +1,20 @@
 import type { GetObjectCommandOutput } from "@aws-sdk/client-s3";
 
-interface ParsedHighlight {
+export interface ParsedHighlight {
 	content: string;
 	[key: string]: unknown;
 }
 
-interface ParsedHighlightWithHash extends ParsedHighlight {
+export interface ParsedHighlightWithHash extends ParsedHighlight {
 	hash: number;
 }
 
-interface ParsedVolume {
+export interface ParsedVolume {
 	authors: string[];
 	title: string;
 }
 
-type Importer = (mail: GetObjectCommandOutput["Body"]) => Promise<{
+export type Importer = (mail: GetObjectCommandOutput["Body"]) => Promise<{
 	highlights: ParsedHighlight[];
 	volume: ParsedVolume;
 }>;
